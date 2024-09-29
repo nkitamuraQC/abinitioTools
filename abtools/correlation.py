0d64431baacf7d8ea7df69c1c6b00f568dc8c308
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-09-29 16:54:35 +0900

    Update correlation.py

--- abtools/correlation.py
+++ abtools/correlation.py
@@ -74,3 +74,63 @@ class AbinitioToolsClass:
         green = np.einsum("an,bn,on->abo", mo, mo, 1./denom)
         return green
 
+　　　def calc_chg_corr(self, site_i, site_j):
+        if self.dm1 is not None:
+            dm1 = self.dm1
+        if self.dm2 is not None:
+            dm2 = self.dm2
+        if calc_type == "scf":
+            if self.dm1 is None:
+                dm1 = self.mf.make_rdm1()
+            if self.dm2 is None:
+                dm2 = self.mf.make_rdm2()
+            hcore = self.mol.intor('cint1e_kin_sph') + self.mol.intor('cint1e_nuc_sph')
+            hcore = hcore[site_i, site_j]
+        if calc_type == "ccsd":
+            if self.dm1 is None:
+                dm1 = self.mycc.make_rdm1()
+            if self.dm2 is not None:
+                dm2 = self.mycc.make_rdm2()
+            mo = self.mf.mo_coeff
+            hcore = self.mol.intor('cint1e_kin_sph') + self.mol.intor('cint1e_nuc_sph')
+            hcore = hcore[site_i, site_j]
+            dm2 = np.einsum("ai,bj,ck,dl,ijkl->abcd", mo, mo, mo, mo, dm2)
+        corr = 0
+        norb = dm1.shape[0]
+        identity = np.identity(norb)
+        if site_i == site_j:
+            corr += dm1[0][site_i, site_i] + dm1[1][site_i, site_i]
+        i = site_i
+        j = site_j
+        corr += dm2[0][i,j,j,i] + dm2[1][i,i,j,j] + dm2[1][j,j,i,i] - dm2[2][i,j,j,i]
+        corr -= (dm1[0][i,i] + dm1[1][i,i]) * (dm1[0][j,j] + dm1[1][j,j])
+        return corr
+
+def calc_spin_corr(fci_vec,norb,nelec):
+    delta = numpy.identity(norb,dtype=float)
+    rdm1_,rdm2_ = fci.direct_spin1.make_rdm12s(fci_vec,norb, nelec)
+    rdm1,rdm2 = [],[]
+    for i in range(len(rdm1_)):
+        rdm1.append(rdm1_[i])
+    for i in range(len(rdm2_)):
+        rdm2.append(rdm2_[i])
+    rdm2_aaaa = numpy.zeros((norb,norb))
+    rdm2_bbbb = numpy.zeros((norb,norb))
+    rdm2_abba = numpy.zeros((norb,norb))
+    rdm2_baab = numpy.zeros((norb,norb))
+    rdm2_diagonal = numpy.zeros((norb,norb))
+    for i in range(norb):
+        for j in range(norb):
+            #rdm2_diagonal[i,j] = rdm2[0][i,j,j,i] + rdm2[2][i,j,j,i] + rdm2[1][i,j,j,i]
+            #rdm_ = rdm2[1].T.conj()
+            #rdm2_diagonal[i,j] += rdm_[i,j,j,i]
+            rdm2_aaaa[i,j] = delta[i,j]*rdm1[0][j,i] - rdm2[0][i,j,j,i]
+            rdm2_bbbb[i,j] = delta[i,j]*rdm1[1][j,i] - rdm2[2][i,j,j,i]
+            rdm2_abba[i,j] = -rdm2[1][i,i,j,j]
+            rdm_ = numpy.einsum("pqrs -> rspq",rdm2[1])
+            rdm_ = rdm_.conj()
+            rdm2_baab[i,j] = -rdm_[i,i,j,j]
+    spin_corr = rdm2_aaaa + rdm2_bbbb +  rdm2_abba + rdm2_baab
+    #spin_corr = rdm2_diagonal
+    return spin_corr,rdm1,rdm2
+
