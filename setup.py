8158deb6db3087468d95fcf214a1f5fb3feff603
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-09-29 17:11:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,10 @@ def load_requirements(file_name):
 
 # パッケージのメタデータと依存関係を定義
 setup(
-    name='your_package_name',  # パッケージ名
+    name='abinitioTools',  # パッケージ名
     version='0.1',             # バージョン
-    description='A sample Python package',  # 説明
     packages=find_packages(),   # パッケージの自動検出
     install_requires=load_requirements('requirements.txt'),  # 依存関係を指定
-    author='Your Name',         # 作者情報
-    author_email='your.email@example.com',  # 作者のメールアドレス
+    author='Naoki Kitamura',         # 作者情報
     url='https://example.com/your_package',  # プロジェクトのURL
 )
