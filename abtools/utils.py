fc44790fa951a414423b5c5b2af929aeaee45674
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-09-29 17:09:05 +0900

    Update utils.py

--- abtools/utils.py
+++ abtools/utils.py
@@ -1,1 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+def plot_ao(mf, data):
+    ao_labels = mf.mol.ao_labels()
+    assert(data.shape[0] == len(ao_labels))
+    assert(data.shape[1] == len(ao_labels))
+
+    df = pd.DataFrame(data, columns=ao_labels, index=ao_labels)
+    sns.heatmap(df, cmap='coolwarm', annot=True)
+    plt.show()
+    return
+  
